0cf018b51b138891e663a8479f21d393a899c4ec
Hyungju LEE <hyungjuice@naver.com>
2022-11-17 17:28:23 +0900

    small update
    메인 함수가 너무 커져서 외부 함수로 전환

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -19,39 +19,9 @@ func checkInput() {
                 print("DEBUG: input from data biding", intInput)
                 switch intInput {
                 case 1:
-                    print("추가할 학생의 이름을 입력해주세요")
-                    var nameInput = readLine()
-                    if let nameInput = nameInput, studentsArray.contains(where: { $0[0] == nameInput }) {
-                        print("\(nameInput)은 이미 존재하는 이름입니다. 추가하지 않습니다.")
-                        checkInput()
-                    } else if let nameInput = nameInput, nameInput.trimmingCharacters(in: .whitespaces) != "" {
-                        studentInfoArray.append(nameInput)
-                        studentsArray.append(studentInfoArray)
-                        studentInfoArray.removeAll()
-                        print("DEBUG: studentsArray", studentsArray)
-                        print("\(nameInput) 학생을 추가했습니다.")
-                        checkInput()
-                    } else if let nameInput = nameInput, nameInput.trimmingCharacters(in: .whitespaces) == "" {
-                        print("입력이 잘못되었습니다. 다시 확인해주세요.")
-                        checkInput()
-                    }
+                    addingStudent()
                 case 2:
-                    var nameInput = readLine()
-                    print("삭제할 학생의 이름을 입력해주세요")
-                    if let nameInput = nameInput {
-                        print("DEBUG: studentsArray", studentsArray)
-                        if studentsArray.contains(where: { $0[0] == nameInput  }) {
-                            var studentIndexInArray = studentsArray.firstIndex(where: { $0[0] == nameInput })!
-                            studentsArray.remove(at: studentIndexInArray)
-                            print("\(nameInput)학생을 삭제하였습니다.")
-                            print("DEBUG: studentsArray", studentsArray)
-                            checkInput()
-                        } else {
-                            print("\(nameInput) 학생을 찾지 못했습니다.")
-                            print("DEBUG: studentsArray", studentsArray)
-                            checkInput()
-                        }
-                    }
+                    deletingStudent()
 //                case 3:
 //                case 4:
 //                case 5:
@@ -70,6 +40,44 @@ func checkInput() {
     } else {
         print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.");
         checkInput()
+    }
+}
+
+func addingStudent() {
+    print("추가할 학생의 이름을 입력해주세요")
+    var nameInput = readLine()
+    if let nameInput = nameInput, studentsArray.contains(where: { $0[0] == nameInput }) {
+        print("\(nameInput)은 이미 존재하는 이름입니다. 추가하지 않습니다.")
+        checkInput()
+    } else if let nameInput = nameInput, nameInput.trimmingCharacters(in: .whitespaces) != "" {
+        studentInfoArray.append(nameInput)
+        studentsArray.append(studentInfoArray)
+        studentInfoArray.removeAll()
+        print("DEBUG: studentsArray", studentsArray)
+        print("\(nameInput) 학생을 추가했습니다.")
+        checkInput()
+    } else if let nameInput = nameInput, nameInput.trimmingCharacters(in: .whitespaces) == "" {
+        print("입력이 잘못되었습니다. 다시 확인해주세요.")
+        checkInput()
+    }
+}
+
+func deletingStudent() {
+    var nameInput = readLine()
+    print("삭제할 학생의 이름을 입력해주세요")
+    if let nameInput = nameInput {
+        print("DEBUG: studentsArray", studentsArray)
+        if studentsArray.contains(where: { $0[0] == nameInput  }) {
+            var studentIndexInArray = studentsArray.firstIndex(where: { $0[0] == nameInput })!
+            studentsArray.remove(at: studentIndexInArray)
+            print("\(nameInput)학생을 삭제하였습니다.")
+            print("DEBUG: studentsArray", studentsArray)
+            checkInput()
+        } else {
+            print("\(nameInput) 학생을 찾지 못했습니다.")
+            print("DEBUG: studentsArray", studentsArray)
+            checkInput()
+        }
     }
 }
 
