48a4d7dee4c70f612c9e3b8a103e30552834171c
Hyungju LEE <hyungjuice@naver.com>
2022-11-18 16:20:25 +0900

    Adding, changing and deleting credits
    * 특이사항 issue에 정리

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -8,22 +8,109 @@
 import Foundation
 
 var studentsArray: [[String]] = []
-var studentDic: [String:String] = [:]
 var studentInfoArray: [String] = []
+
+enum CreditPoints: String {
+    case Aplus = "A+"
+    case A = "A"
+    case Bplus = "B+"
+    case B = "B"
+    case Cplus = "C+"
+    case C = "C"
+    case Dplus = "D+"
+    case D = "D"
+}
+/*
+func calculateCredits() {
+//    switch CreditPoints.self {
+//    case Aplus:
+//        return 4.5
+//    }
+//
+    //    case A = 4.0
+    //    case Bplus = 3.5
+    //    case B = 3.0
+    //    case Cplus = 2.5
+    //    case C = 2.0
+    //    case Dplus = 1.5
+    //    case D = 1.0
+}
+*/
 
 func checkInput() {
     print("원하는 기능을 입력해주세요.\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
     var input = readLine()
     if let input = input, let intInput = Int(input) {
             if 1...5 ~= intInput {
-                print("DEBUG: input from data biding", intInput)
                 switch intInput {
                 case 1:
                     addingStudent()
                 case 2:
                     deletingStudent()
-//                case 3:
-//                case 4:
+                case 3:
+                    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+                    var creditInput = readLine()
+                    if let creditInput = creditInput {
+                        var splittedInputArray = creditInput.split { $0 == " " }
+                        
+                        if splittedInputArray.endIndex < 3 {
+                            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+                            checkInput()
+                        } else if CreditPoints(rawValue: String(splittedInputArray[2])) == nil{
+                            print("점수 입력이 잘못되었습니다. 다시 확인해주세요.")
+                            checkInput()
+                        } else if !studentsArray.contains(where: { $0[0] == splittedInputArray[0] }) {
+                            print("\(splittedInputArray[0]) 학생을 찾지 못했습니다.")
+                            checkInput()
+                            // 정상 추가/변경되는 경우
+                        } else if studentsArray.contains(where: { $0[0] == splittedInputArray[0] }) {
+                            if studentsArray.contains(where: { $0.endIndex == 1 }) {
+                                // 성적이 아무것도 없을때
+                                var studentIndex = studentsArray.firstIndex(where: { $0[0] == splittedInputArray[0]  })!
+                                studentsArray[studentIndex].append(String(splittedInputArray[1]))
+                                studentsArray[studentIndex].append(String(splittedInputArray[2]))
+                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 과목이 \(splittedInputArray[2])로 추가(변경) 되었습니다.")
+//                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 과목이 \(splittedInputArray[2])로 추가 되었습니다.")
+                                checkInput()
+                            } else if studentsArray.contains(where: { $0[1] == splittedInputArray[1] }) {
+                                // 같은 과목의 성적이 있을 떼
+                                var studentAndSubjectIndex = studentsArray.firstIndex(where: { $0[0] == splittedInputArray[0] && $0[1] == splittedInputArray[1]  })!
+                                studentsArray[studentAndSubjectIndex].remove(at: 2)
+                                studentsArray[studentAndSubjectIndex].append(String(splittedInputArray[2]))
+                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 과목이 \(splittedInputArray[2])로 추가(변경) 되었습니다.")
+//                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 과목이 \(splittedInputArray[2])로 변경 되었습니다.")
+                                checkInput()
+                            }
+                        }
+                    }
+                case 4:
+                    print("성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift")
+                    var creditInput = readLine()
+                    if let creditInput = creditInput {
+                        var splittedInputArray = creditInput.split { $0 == " " }
+                        print("DEBUG: splittedInputArray :", splittedInputArray)
+                        
+                        if splittedInputArray.endIndex < 2 {
+                            print("입력이 잘못되었습니다. 다시 확인해주세요.")
+                            checkInput()
+                        } else if !studentsArray.contains(where: { $0[0] == splittedInputArray[0] }) {
+                            print("\(splittedInputArray[0]) 학생의 해당 과목 점수를 찾지 못했습니다.")
+                            checkInput()
+                        } else if studentsArray.contains(where: { $0[0] == splittedInputArray[0] }) {
+                            if studentsArray.contains(where: { $0[1] == splittedInputArray[1] }) {
+                                var studentAndSubjectIndex = studentsArray.firstIndex(where: { $0[0] == splittedInputArray[0] && $0[1] == splittedInputArray[1]  })!
+                                // 배열의 내용 뿐 아니라 데이터 공간까지 지워야 함. 안그러면 빈 배열이 남아 차후 오류...
+                                studentsArray[studentAndSubjectIndex].removeAll()
+                                studentsArray.removeAll(where: { $0.isEmpty })
+                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 과목의 성적이 삭제 되었습니다.")
+                                checkInput()
+                            } else {
+                                print("\(splittedInputArray[0]) 학생을 찾지 못했습니다.")
+//                                print("\(splittedInputArray[0]) 학생의 \(splittedInputArray[1]) 성적을 찾지 못했습니다.")
+                                checkInput()
+                            }
+                        }
+                    }
 //                case 5:
                 default:
                     break
@@ -46,6 +133,7 @@ func checkInput() {
 func addingStudent() {
     print("추가할 학생의 이름을 입력해주세요")
     var nameInput = readLine()
+    print(studentsArray)
     if let nameInput = nameInput, studentsArray.contains(where: { $0[0] == nameInput }) {
         print("\(nameInput)은 이미 존재하는 이름입니다. 추가하지 않습니다.")
         checkInput()
@@ -63,8 +151,8 @@ func addingStudent() {
 }
 
 func deletingStudent() {
-    var nameInput = readLine()
     print("삭제할 학생의 이름을 입력해주세요")
+    var nameInput = readLine()
     if let nameInput = nameInput {
         print("DEBUG: studentsArray", studentsArray)
         if studentsArray.contains(where: { $0[0] == nameInput  }) {
@@ -80,6 +168,8 @@ func deletingStudent() {
         }
     }
 }
+
+
 
 checkInput()
 
